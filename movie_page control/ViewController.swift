b44ce88dcd24364e27fe94b45f8e9755b6c23404
yousun <a0921990065@gmail.com>
2021-04-25 15:36:35 +0800

    Initial Commit

---
+++ movie_page control/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  movie_page control
+//
+//  Created by yousun on 2021/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
